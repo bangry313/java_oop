9c644033575fc7443e9351815deff80c66517202
bangry313 <bangry313@gmail.com>
2023-02-09 16:08:41 +0900

    3rd commit

--- src/oop/Test2.java
+++ src/oop/Test2.java
@@ -5,6 +5,9 @@ public class Test2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("깃헙 연습니다......");
+		
+		
+		System.out.println("희원이 바보입니다....");
 
 	}
 
